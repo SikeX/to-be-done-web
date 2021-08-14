bac8ffe9c1f654a64e710288ffe1637a557961fc
SikeX <yoloxz719@gmail.com>
2021-08-14 15:30:27 +0800

    feat: uiupdate

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -5,7 +5,7 @@ import TaskStat from '../components/taskStat'
 
 const Todo = () => {
     return (
-        <div className='flex h-screen'>
+        <div className='flex h-full'>
             <div className='flex flex-col h-screen w-full bg-blue-100
             md:bg-white md:w-1/5'>
                 <Banner />
