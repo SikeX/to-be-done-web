f4ff54e0c83939cdb4518ece453498df37a0ab1b
SikeX <yoloxz719@gmail.com>
2021-08-14 14:13:26 +0800

    feat: uiupdate

--- src/Routes.js
+++ src/Routes.js
@@ -3,13 +3,15 @@ import Login from './pages/login'
 import About from './pages/about'
 import Todo from './pages/todo'
 
-// import createHistory from 'history/createBrowserHistory'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createHashHistory'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
+const history = createHistory()
 
 
 function Routes() {
   return (
-    <Router>
+    <Router history={history}>
       <Switch>
           <Route exact path='/' component={Login} />
 
